4ffee0821582a9400c6171e00e4fb5b800e514be
Your Name <you@example.com>
2021-03-04 15:18:34 -0800

    commit

---
+++ py backup/range.py
@@ -0,0 +1,25 @@
+#for x in range(6):
+#    print(x)
+
+#^^ Since x isn't put to anything, it prints all of them from 0 - 5 
+    #because it always starts at 0 and 6 characters goes to 5
+    #x will print 0, 1, 2, 3, 4, 5
+
+#------------------------------------------------------------
+
+#for x in range(2, 6):
+#    print(x)
+
+#^^ x is in the range of 2 - 6. Which means the second character to the 6th character
+    #x will print to be 2, 3, 4, 5
+
+#------------------------------------------------------------
+
+#for x in range(2, 30, 3):
+#    print(x)
+
+#^^ the range is between 2 and 30. The third letter is the incriment of looping. 
+    #instead of going 1 by 1, it goes 3 by 3 i.e., 2, 5, 8, etc. 
+
+#------------------------------------------------------------
+
