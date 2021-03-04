961bb1e8c764f1c475388b9ff46333afe701b0da
Your Name <you@example.com>
2021-03-04 15:29:51 -0800

    commit

---
+++ py backup/functions.py
@@ -0,0 +1,14 @@
+#NTOES FOR FUNCTIONS
+
+"""
+def myFunction():
+    print("how about no")
+
+myFunction()
+"""
+
+    #since you defined a function that has the order to execute a print command
+        #you only need to write "myFunction()" to process the whole block of code. 
+
+#------------------------------------------------------------
+
--- py backup/range.py
+++ py backup/range.py
@@ -1,22 +1,26 @@
+"""
 #for x in range(6):
 #    print(x)
+"""
 
 #^^ Since x isn't put to anything, it prints all of them from 0 - 5 
     #because it always starts at 0 and 6 characters goes to 5
     #x will print 0, 1, 2, 3, 4, 5
 
 #------------------------------------------------------------
-
+"""
 #for x in range(2, 6):
 #    print(x)
+"""
 
 #^^ x is in the range of 2 - 6. Which means the second character to the 6th character
     #x will print to be 2, 3, 4, 5
 
 #------------------------------------------------------------
-
+"""
 #for x in range(2, 30, 3):
 #    print(x)
+"""
 
 #^^ the range is between 2 and 30. The third letter is the incriment of looping. 
     #instead of going 1 by 1, it goes 3 by 3 i.e., 2, 5, 8, etc. 
@@ -24,11 +28,12 @@
 #------------------------------------------------------------
 
 #ELSE IN FOR LOOP
-
+"""
 #for x in range(6):
 #    print(x)
 #else: 
 #    print("finally finished")
+"""
 
 #^^ since the first part of the code is to print the range of 0-5 (6 characters)
     # once x does not print a number anymore, it will print the msg "finally finished" 
@@ -38,11 +43,14 @@
 
 #BREAK THE LOOP
 
+"""
 #for x in range(6):
 #    if x == 3: break
 #    print(x)
 #else: 
 #    print("done")
+"""
+
 
 #if you look at the code and don't know how it reads top to bottom, you would expect the end to print "done"
     #but when you do a "break" statement. the rest of the code does NOT get executed. 
@@ -54,9 +62,11 @@
 #adj = ["red", "big", "tasty"]
 #fruits = ["apple", "banana", "cherry"]
 
-#for x in adj:
-#    for y in fruits:
-#        print(x, y)
+"""
+for x in adj:
+    for y in fruits:
+        print(x, y)
+"""
 
 #since "for y" is nested inside the adj loop, when print(x, y) is executed, 
     #each word will be printed with each fruit. red apple, red banana, red cherry, etc. 
