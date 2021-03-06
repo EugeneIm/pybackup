83ee3edfd9d793088eb3a093e6e2d133aeaef7a6
EugeneIm <immer277@gmail.com>
2021-03-06 15:35:08 -0800

    commit

--- py backup/inheritance.py
+++ py backup/inheritance.py
@@ -17,7 +17,34 @@ x = Person("John", "Doe")
 x.printname()
 """
 
-#The parent class is "Person" and the child class is "self". 
+#The parent class is "Person" 
 #---------------------------------
 
+#Creating a child class. This one will be named "student" 
+    #but it will inherit the properties and methods from the parent class "person" 
+
+"""
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass      #when you use the "pass" in the syntax
+            #you don't want to add anything else. NOTHING ELSE. 
+
+x = Student("Mike", "Olsen")
+x.printname()
+"""
+
+#since "student" has inhereted the parent class, using x.printname() will print 
+    #Mike Olsen just like how 
+        #x = person("john", "doe")
+        #x.printname() will print out "john doe"
+
+#when you use the "pass" in the syntax
+    #you don't want to add anything else. NOTHING ELSE. 
 
