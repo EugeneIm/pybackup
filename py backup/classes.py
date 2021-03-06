b044017b1d9510c57a11556624923c68492acbe9
EugeneIm <immer277@gmail.com>
2021-03-05 18:07:20 -0800

    commit

---
+++ py backup/classes.py
@@ -0,0 +1,10 @@
+#you create a class in py the same way you would create one in HTML
+    #class myFunction() - py
+    #class = "function" - HTML
+    #class myFunction()
+        #x = 5
+
+#creating an object by using the class above
+    #p1 = myFunction()
+    #print(p1.x)
+        #outcome will be "5"
