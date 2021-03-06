346a6f9fda27d8836222399a30c30b84f4ff556e
EugeneIm <immer277@gmail.com>
2021-03-06 15:29:59 -0800

    commit

--- py backup/inheritance.py
+++ py backup/inheritance.py
@@ -1,3 +1,23 @@
 #Inheritance
 
 #Parent and Child class. 
+
+"""
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+"""
+
+#The parent class is "Person" and the child class is "self". 
+#---------------------------------
+
+
