3f7adc4f80fbd0b599f8d99c92ed5a9bcf0f8b27
EugeneIm <immer277@gmail.com>
2021-03-06 16:00:25 -0800

    commit

--- py backup/inheritancept2.py
+++ py backup/inheritancept2.py
@@ -19,3 +19,24 @@ x = Student("Mike", "Olsen")
 x.printname()
 """
 
+#-------------------------------
+
+#super() Function
+#Python also has a super() function that will make the child class inherit all the methods and properties from its parent:
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+#because there is the "super()" function, it makes the child class inherite the methods and properties from its parent. 
