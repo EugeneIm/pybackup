2eb64477cd01f56cfd1b20b619d8f7412fe66003
Your Name <you@example.com>
2021-04-06 20:49:54 -0700

    commit

--- wk1/demo.py
+++ wk1/demo.py
@@ -1,27 +1,27 @@
 #Demo for lesson 1 
 
-# greeting = "hello and welcome to python"
+greeting = "hello and welcome to python"
 
-# x = 10 
-# x = "hello"
+x = 10 
+x = "hello"
 
-# print(greeting)
-# print(type(x))
+print(greeting)
+print(type(x))
 
-# num_students = 20
-# print('there are', num_students, 'in our class today')
+num_students = 20
+print('there are', num_students, 'in our class today')
 
-# print('there are ' + str(num_students) + ' in our class today')
+print('there are ' + str(num_students) + ' in our class today')
 
-# print('there are %d in our class today' %num_students)
+print('there are %d in our class today' %num_students)
 
-# print('%s \n Here we have %d in our class today' %(greeting, num_students))
+print('%s \n Here we have %d in our class today' %(greeting, num_students))
 
-# user_input = input('say something ho \n')
-# print(user_input)
+user_input = input('say something ho \n')
+print(user_input)
 
-# print('this will', end=' ')
-# print('print on the same line')
+print('this will', end=' ')
+print('print on the same line')
 
 x = 5
 y = 10
@@ -31,4 +31,8 @@ print(x*y)
 print(x, "*", y, "=", z)        #this is concatenating   
 print("%d * %d = %d" %(x, y, z))        #this is using placeholders to re-write it. 
 
+# %s - string
+# %f - float
+# %d - decimal 
 
+#use these to format the string appropriately 
