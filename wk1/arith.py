d42407bf90f7caf69f52f9259bb40e55757012be
Your Name <you@example.com>
2021-04-06 20:19:20 -0700

    commit

---
+++ wk1/arith.py
@@ -0,0 +1,36 @@
+#Arithmetic Operators
+
+# +, -, *, /, // (result will be a whole number, no decimals), ** (exponents), % (modulus, remainder after dividing). 
+
+'''
+x = 11
+y = 2
+
+print(x%y)
+'''
+# above is a modulus example.
+
+'''
+x = 5
+y = 6 + x       #y is 11 now
+z = y - 1       #z is 10 now
+a = y * z       #a is 110 now
+b = 100
+c = b/5         #c is now 20 because 100/5 is 20
+base = 2
+exp = 3
+value = base ** exp 
+'''
+
+'''
+x = 5.5
+y = 6.1 + x
+z = y - 1.1
+
+
+a = 5.6
+b = 11
+c = int(a) + b      #c is now evaluated as 5 + 11
+'''
+#Arithmetic examples above
+
