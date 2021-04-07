0221a82687ca894fd265aca982b8ab38506d8f42
Your Name <you@example.com>
2021-04-06 20:43:57 -0700

    commit

---
+++ wk1/demo.py
@@ -0,0 +1,34 @@
+#Demo for lesson 1 
+
+# greeting = "hello and welcome to python"
+
+# x = 10 
+# x = "hello"
+
+# print(greeting)
+# print(type(x))
+
+# num_students = 20
+# print('there are', num_students, 'in our class today')
+
+# print('there are ' + str(num_students) + ' in our class today')
+
+# print('there are %d in our class today' %num_students)
+
+# print('%s \n Here we have %d in our class today' %(greeting, num_students))
+
+# user_input = input('say something ho \n')
+# print(user_input)
+
+# print('this will', end=' ')
+# print('print on the same line')
+
+x = 5
+y = 10
+z = x * y
+
+print(x*y)
+print(x, "*", y, "=", z)        #this is concatenating   
+print("%d * %d = %d" %(x, y, z))        #this is using placeholders to re-write it. 
+
+
